ef38da8a8a279ee6bf420fe75cd1517a4773724d
nilpatel <nilpatel62@gmail.com>
2023-06-29 09:40:50 +0530

    change the url for api call

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     setIsLoading(true); // Set loading state to true
 
     // Make an API call using axios
-    axios.post('http://127.0.0.1:8000/summarize', { text: inputText })
+    axios.post('https://9cc6-219-91-133-43.ngrok-free.app/summarize', { text: inputText })
       .then((response) => {
         console.log(response.data.summary);
         setResponse(response.data.summary);
